abc88364d8d56ce77cca7ad9cd5aa80b439d114c
Breno <59184811+Breno30@users.noreply.github.com>
2020-08-25 17:10:34 -0300

    Add files via upload

---
+++ bhaskara.1.1.js
@@ -0,0 +1,77 @@
+
+
+
+
+    function bhaskara() {
+
+        var a = window.document.getElementById('txtn1').value
+        var b = window.document.getElementById("txtn2").value
+        var c = window.document.getElementById("txtn3").value
+        //coletando valores
+
+        var delt = b ** 2 - 4 * a * c
+
+
+        //se a for igual a zero
+        if (a == 0) {
+            window.alert(`Toda função do 2º grau tem a ≠ 0`)
+        } else {
+            delta.innerHTML = ` ${delt}`
+            if (delt < 0) {
+                //Δ
+                res1.innerHTML = ` <strong>impossivel, delta negativo</strong>`
+                res2.innerHTML = ` <strong>impossivel, delta negativo</strong>`
+            } else {
+                var x1 = ((- b + (delt ** (1 / 2))) / 2 * a).toFixed(2)
+                var x2 = ((- b - (delt ** (1 / 2))) / 2 * a).toFixed(2)
+                res1.innerHTML = ` <strong>${x1}</strong>`
+                res2.innerHTML = ` <strong>${x2}</strong>`
+                vert.innerHTML = ` <strong>${(-b / (2 * a)).toFixed(2)}, ${(-delt / (4 * a)).toFixed(2)}</strong>`
+            }
+            //condicoes concavidade
+            if (a < 0) {
+                con.innerHTML = ` <strong>baixo</strong>`
+            } else {
+                con.innerHTML = ` <strong>cima</strong>`
+            }
+            //invertendo grafico              
+            if (x1 > x2) {
+                var pri = x2
+                var seg = x1
+            } else {
+                var pri = x1
+                var seg = x2
+            }
+
+
+            //grafico
+            let myChart = document.getElementById('myChart').getContext('2d');
+            let massPopChart = new Chart(myChart, {
+                type: 'line',
+                data: {
+                    //x
+                    labels: [x1, (-b / (2 * a)).toFixed(2), x2],
+
+                    datasets: [{
+                        label: 'x',
+                        //y
+                        data: [0, (-delt / (4 * a)).toFixed(2), 0],
+                        backgroundColor: 'transparent'
+                    }]
+                },
+                options: {
+                    animation: {
+                        //animação dura 1 segundo
+                        duration: 1000
+
+                    }
+
+                }
+            });
+
+        }
+
+
+    }
+
+                
