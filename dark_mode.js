2f89c52da76ae542fcaeea501033e484b225a561
breno30 <brenodonascimentosilvaa@gmail.com>
2022-04-14 14:32:54 -0300

    Ajustando função de cores

--- dark_mode.js
+++ dark_mode.js
@@ -1,34 +1,20 @@
-
 var dark = true;
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    dark = true;
 
-} else {
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    dark = true;
+else
     dark = false;
-}
-muda_cor()
 
 
 function mode() {
-    if (dark) {
-        dark = false;
-    } else {
-        dark = true;
-    }
-    muda_cor()
-}
 
 
-function muda_cor() {
     //muda cor da pagina
     if (dark) {
         document.body.style.backgroundColor = '#232931';
         document.body.style.color = 'white';
         document.getElementById('mode_front').style.backgroundColor = '#232931';
         document.getElementById('mode_front').style.left = '10px';
-
-  
-
 
     } else {
         document.body.style.backgroundColor = 'white';
@@ -39,11 +25,6 @@ function muda_cor() {
 
     }
 
+    dark = !dark;
+
 }
-
-
-
-
-    
-
-                
